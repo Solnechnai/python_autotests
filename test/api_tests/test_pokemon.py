4315e7b58b3050096f29ed3cb0e9fdd9ec5ccb2b
Tatyana <gol8an@yandex.com>
2024-11-21 12:32:29 +0300

    update test status code 200

--- test/api_tests/test_pokemon.py
+++ test/api_tests/test_pokemon.py
@@ -6,7 +6,7 @@ Url = "https://api.pokemonbattle.ru/"
 
 # проверка статус-кода запроса информации о тренере
 def test_status_code():
-    response = requests.get(f"{Url}trainers", params={"trainer_id": 8549})
+    response = requests.get(f"{Url}v2/trainers", headers={'trainer_token': '0c433c5ee3cd92367729a5c068a118a2'}, params={"trainer_id": 8549})
     assert response.status_code == 200
 
 # проверка имени и города тренера
